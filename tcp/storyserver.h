c28c5b02633d1e2e2117bed95d01935151c95196
david-bcarvalho <dmbfc11@gmail.com>
2017-05-18 19:07:52 +0100

    new stable .zip

--- tcp/storyserver.h
+++
@@ -1,8 +0,0 @@
-//#define SOCKET_NAME "xxx"
-#define MESSAGE_LEN 100
-//#define SOCK_ADDRESS "/tmp/sock_16"
-#define SOCKET_NAME "my_socket"
-
-typedef struct message{
-    char buffer[MESSAGE_LEN];
-} message;
